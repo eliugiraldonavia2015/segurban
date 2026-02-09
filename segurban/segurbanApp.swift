7d76bf4256a5f76bfa1607a305bf8f5c35d9e3bc
eliu giraldo <eliugiraldonavia2015@gmail.com>
2026-02-08 22:22:13 -0500

    Initial Commit

---
+++ segurban/segurbanApp.swift
@@ -0,0 +1,17 @@
+//
+//  segurbanApp.swift
+//  segurban
+//
+//  Created by eliu giraldo on 8/2/26.
+//
+
+import SwiftUI
+
+@main
+struct segurbanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
